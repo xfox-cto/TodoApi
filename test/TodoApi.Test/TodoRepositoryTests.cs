c90a212de8793aea5f0b8f583763e462affb8986
xfox.cto <xfox.cto@live.com>
2016-10-14 10:01:43 +0800

    new test

---
+++ test/TodoApi.Test/TodoRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoApi.Controllers;
+using TodoApi.Models;
+
+namespace TodoApi.Test
+{
+    [TestClass]
+    public class TodoRepositoryTests
+    {
+        [TestMethod]
+        public void Add()
+        {
+            ITodoRepository repository = new TodoRepository();
+            repository.Add(new TodoItem()
+            {
+                Name = "Test",
+                IsComplete = true,
+            });
+
+            var len = repository.GetAll().ToArray().Length;
+            Assert.AreEqual(2, len);
+        }
+    }
+}
